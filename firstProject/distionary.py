4548749467743c75f33a1e1a58384dd8d8235808
Saurav Sinha <sauravsinha76@gmail.com>
2022-01-21 17:55:15 +0530

    New code

---
+++ firstProject/distionary.py
@@ -0,0 +1,3 @@
+person1_information = {'city': 'San Francisco', 'name': 'Sam', "food": "shrimps"}
+for k, v in person1_information.items():
+    print("Key:"+k+",Value:"+v)
---
+++ firstProject/funation_call.py
@@ -0,0 +1,6 @@
+def add_two_numbers(num1, num2):
+    result = num1 + num2
+    return result
+
+
+print(add_two_numbers(2.1, '6'))
